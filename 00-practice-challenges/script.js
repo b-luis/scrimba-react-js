5902515448b749880442cae98bb8370d2ded33f6
b-luis <bscarletluis@gmail.com>
2023-07-18 01:30:33 +0800

    feat: react challenge

--- 00-practice-challenges/script.js
+++ 00-practice-challenges/script.js
@@ -103,4 +103,41 @@ appending an h1 to our div#root (without using innerHTML).
 // h1El.className = 'header';
 // div.append(h1El);
 
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+//////////////////////////////////////////////////////////////
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const Navbar = (
+    <nav>
+        <h1>textractify</h1>
+        <ul>
+            <li>About</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    Navbar,
+    document.getElementById('root')
+);
+
 
