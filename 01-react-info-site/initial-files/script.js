c9ea17cd95e094c538b8313d6371479679244146
b-luis <bscarletluis@gmail.com>
2023-07-18 00:29:09 +0800

    first react practice

---
+++ 01-react-info-site/initial-files/script.js
@@ -0,0 +1,14 @@
+// first react practice
+/* 
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside
+*/
+
+ReactDOM.render(
+    <ul>
+        <li>JavaScript</li>
+        <li>ReactJS</li>
+    </ul>,
+    document.getElementById('root')
+);
+
