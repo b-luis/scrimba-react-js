8fd96bd771ad2126f9685baea493e5416e3e3acf
b-luis <bscarletluis@gmail.com>
2023-07-18 16:04:23 +0800

    feat: initial project files

--- 01-react-info-site/src/main.jsx
+++ 01-react-info-site/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Message from './components/Message.jsx'
-import './styles/Message.css'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Message />
+    <App />
   </React.StrictMode>,
 )
