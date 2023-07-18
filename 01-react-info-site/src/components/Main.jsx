ff173315be8e6d7de3be762314dedb630e46549e
b-luis <bscarletluis@gmail.com>
2023-07-18 20:15:18 +0800

    feat: project setup

---
+++ 01-react-info-site/src/components/Main.jsx
@@ -0,0 +1,17 @@
+
+function Main(){
+    return (
+        <>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </>
+    )
+}
+
+export default Main;
---
+++ 01-react-info-site/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import logo from '../assets/react-logo.png'
+
+function Navbar() {
+    return (
+        <>
+            {/* <h1>Navbar goes here</h1> */}
+            <img src={logo} alt="react-logo" />
+            <h3></h3>
+            <h4></h4>
+        </>
+    )
+}
+
+export default Navbar;
