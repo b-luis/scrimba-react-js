13faae5b9ab913aa74fdfac1aa7c099d1ab6cd02
b-luis <bscarletluis@gmail.com>
2023-07-20 22:16:43 +0800

    update: suppressed warning

--- 02-airbnb-clone/src/App.jsx
+++ 02-airbnb-clone/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import 'bulma/css/bulma.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <section className="section">
+ <div className="container">
+   <h1 className="title"> Hello World </h1>
+   <p className="subtitle"> My React app  with     <strong>Bulma</strong> </p>
+ </div>
+</section>
+)
 }
 
 export default App
