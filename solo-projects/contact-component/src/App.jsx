b5d13679ca410b37e3cd382445fb5edec899bb96
b-luis <bscarletluis@gmail.com>
2023-07-24 23:11:38 +0800

    feat: challenge: contact component (reusable) - dist

--- solo-projects/contact-component/src/App.jsx
+++ solo-projects/contact-component/src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './components/Contact'
 function App() {
 
   return (
-    <div className='fixed top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 flex flex-wrap gap-8 justify-center sm:w-11/12'>
+    <div className='fixed top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 flex flex-wrap gap-8 justify-center sm:w-6/12'>
       <Contact
         name="Mr.Whiskerson"
         phone="(212) 555-1234"
--- solo-projects/contact-component/src/components/Contact.jsx
+++ solo-projects/contact-component/src/components/Contact.jsx
@@ -5,14 +5,14 @@ function Contact({name, phone, email, image, alt}) {
     return (
         <div className="bg-gradient-to-l to-zinc-400 hover:from-zinc-400 hover:to-gray-100 cursor-pointer p-4 w-50 pb-6 sm:max-w-sm rounded-lg shadow-2xl">
             <img className="w-full h-auto rounded-lg" src={image} alt={alt} />
-            <h3 className='font-bold text-2xl py-4'>{name}</h3>
+            <h3 className='font-bold text-xl py-4'>{name}</h3>
             <div className='flex items-center'>
                 <img className='h-3 mr-2' src={phoneIcon} alt={alt} />
-                <p className='text-md'>{phone}</p>
+                <p className='text-xs'>{phone}</p>
             </div>
             <div className='flex items-center'>
                 <img className='h-3 mr-2' src={mailIcon} alt={alt} />
-                <p className='text-md'>{email}</p>
+                <p className='text-xs'>{email}</p>
             </div>
         </div>
     )
