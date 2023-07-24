c7e0411660cc33997630b1144da6e952071976e9
b-luis <bscarletluis@gmail.com>
2023-07-24 23:02:54 +0800

    feat: challenge: contact component (reusable)

---
+++ solo-projects/contact-component/src/App.jsx
@@ -0,0 +1,35 @@
+import whiskerson from './assets/mr-whiskerson.png';
+import fluffykins from './assets/fluffykins.png'
+import felix from './assets/felix.png'
+import pumpkin from './assets/pumpkin.png'
+import Contact from './components/Contact'
+
+function App() {
+
+  return (
+    <div className='fixed top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 flex flex-wrap gap-8 justify-center sm:w-11/12'>
+      <Contact
+        name="Mr.Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+        image={whiskerson}/>
+      <Contact
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+        image={fluffykins}/>
+      <Contact
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+        image={felix}/>
+      <Contact
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="thecat@hotmail.com"
+        image={pumpkin}/>
+    </div>
+  )
+}
+
+export default App
---
+++ solo-projects/contact-component/src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import phoneIcon from '../assets/phone-icon.png';
+import mailIcon from '../assets/mail-icon.png';
+
+function Contact({name, phone, email, image, alt}) {
+    return (
+        <div className="bg-gradient-to-l to-zinc-400 hover:from-zinc-400 hover:to-gray-100 cursor-pointer p-4 w-50 pb-6 sm:max-w-sm rounded-lg shadow-2xl">
+            <img className="w-full h-auto rounded-lg" src={image} alt={alt} />
+            <h3 className='font-bold text-2xl py-4'>{name}</h3>
+            <div className='flex items-center'>
+                <img className='h-3 mr-2' src={phoneIcon} alt={alt} />
+                <p className='text-md'>{phone}</p>
+            </div>
+            <div className='flex items-center'>
+                <img className='h-3 mr-2' src={mailIcon} alt={alt} />
+                <p className='text-md'>{email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
