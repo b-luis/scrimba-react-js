21d12dde94c7cde318c7352f18743be5ee2d11c2
b-luis <bscarletluis@gmail.com>
2023-07-27 15:37:56 +0800

    feat: props spread

--- 02-airbnb-clone/src/App.jsx
+++ 02-airbnb-clone/src/App.jsx
@@ -3,39 +3,20 @@ import Hero from './components/Hero'
 import Card from './components/Card'
 import data from '../data/data'
 
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
-
-
 function App() {
-  const cards = data.map((props) => {
-    return <Card
-      img={props.coverImg}
-      rating={props.stats.rating}
-      reviewCount={props.stats.reviewCount}
-      location = {props.location}
-      title = {props.title}
-      price = {props.price}
-    />
-  })
   return (
     <div className='mx-auto sm:w-3/6 lg:w-2/6 font-poppins'>
       <Navbar/> 
       <Hero/>
       <div className='flex overflow-x-auto space-x-4 p-4'>
-        {cards}
+      { 
+        data.map((item) => (
+          <Card key={item.id} {...item} />
+        ))
+      }
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
--- 02-airbnb-clone/src/components/Card.jsx
+++ 02-airbnb-clone/src/components/Card.jsx
@@ -1,41 +1,29 @@
 import star from '../assets/star.png'
-
-/*
-TODO: Build the Card component. For now, hard-code in the data (like the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-/*
-TODO: Pass props to the Card component and display that data
-*  - img ("katie-zaferes.png")
-*  - rating ("5.0")
-*  - reviewCount (6)
-*  - country (Whatever you want)
-*  - title ("Life Lessons with Katie Zaferes")
-*  - price (136)
-*/
-
+/**
+ * 
+ * * props 
+ * * when you have a site, there's no developer that waits for a new 
+ * 
+ */
 function Card(props) {
+    let badgeText;
+    props.openSpots === 0 
+      ? badgeText = 'SOLD OUT' 
+      : props.location === "Online" 
+      ? badgeText = 'ONLINE' 
+      : null;
+
     return (
-            <section className='flex-shrink-0'>
-                <img className='rounded-lg w-full object-cover h-56' src={props.img} />
+            <section className='flex-shrink-0 relative'>
+              <div className="absolute bg-white top-2 left-2 px-1 rounded-sm text-xs capitalize font-semibold">
+                {badgeText}
+              </div>
+                <img className='rounded-lg w-full object-cover h-56' src={props.coverImg} />
                 <div className='flex items-center space-x-1 py-2'>
                     <img src={star} alt="star icon" className='h-3' />
-                    <span className='text-xs'>{props.rating}</span>
-                    <span className='text-xs text-gray-500'>({props.reviewCount}) • </span>
-                    <span className='text-xs text-gray-500'>{props.country}</span>
+                    <span className='text-xs'>{props.stats.rating}</span>
+                    <span className='text-xs text-gray-500'>({props.stats.reviewCount}) • </span>
+                    <span className='text-xs text-gray-500'>{props.location}</span>
                 </div>
                 <p className='text-xs overflow-hidden truncate'>{props.title}</p>
                 <p className='text-[13px] py-1'><span className='font-bold'>From ${props.price}</span> / person</p>
