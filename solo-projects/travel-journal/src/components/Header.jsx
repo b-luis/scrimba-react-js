ff2baa4ec92fef4dd81b3647a9df69b7be347759
b-luis <bscarletluis@gmail.com>
2023-07-29 02:03:21 +0800

    feat: travel journal - header

--- solo-projects/travel-journal/src/components/Header.jsx
+++ solo-projects/travel-journal/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import { FaBeer } from "react-icons/fa";
+import { BsGlobeAmericas } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 function Header(){
     return (
-        <>
-            <header className="">
-                <h1 className=""><FaBeer />my travel journal</h1>
-            </header>
-        </>
+        <header className="flex p-4 bg-[#F55A5A] text-white items-center justify-center">
+            <IconContext.Provider value={{className: "text-2xl" }}>
+                <BsGlobeAmericas />
+            </IconContext.Provider>
+            <h1 className="text-sm px-2">my travel journal.</h1>
+        </header>
     )
 }
 
