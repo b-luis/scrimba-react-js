97f70eba2b49731bad46f2bdd3ed9e4c49a48ff6
b-luis <bscarletluis@gmail.com>
2023-08-12 02:45:47 +0800

    feat: conditional rendering - ternary

--- 00-practice-challenges/props-practice/src/components/Joke.jsx
+++ 00-practice-challenges/props-practice/src/components/Joke.jsx
@@ -19,7 +19,7 @@ function Joke(props) {
         <div className="mx-auto max-w-sm px-6 py-8 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-5">
             {props.setup && <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.setup}</h5>}
             {isShown && <p className="mt-5 font-normal text-xl text-gray-700 dark:text-gray-400">{props.punchline}</p>}
-            <button onClick={toggle} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-3 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Show Joke</button>
+            <button onClick={toggle} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-3 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{isShown ? 'Hide' : 'Show'} Joke</button>
         </div>
     )
 }
