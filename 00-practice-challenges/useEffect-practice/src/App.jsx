96770631d1f7a0611f105eec716e90f84059e21c
b-luis <bscarletluis@gmail.com>
2023-08-19 20:16:34 +0800

    feat: window tracker

---
+++ 00-practice-challenges/useEffect-practice/src/App.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react"
+import WindowTracker from "../data/WindowTracker.jsx"
+
+export default function App() {
+    /**
+     * Challenge:
+     * 1. Create state called `show`, default to `true`
+     * 2. When the button is clicked, toggle `show`
+     * 3. Only display `<WindowTracker>` if `show` is `true`
+     */
+    
+    const [show, setShow] = useState(true)
+    
+    function toggle() {
+        setShow(prevShow => !prevShow)
+    }
+    
+    return (
+        <div className="container">
+            <button onClick={toggle}>
+                Toggle WindowTracker
+            </button>
+            {show && <WindowTracker />}
+        </div>
+    )
+}
