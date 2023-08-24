7cda04e44d91bc7a5b7c71da554c524df5f8bf68
b-luis <bscarletluis@gmail.com>
2023-08-24 10:34:41 +0800

    feat: state for light/dark mode and handling toggle

--- 04-light-dark-mode/src/App.jsx
+++ 04-light-dark-mode/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    const [dark, setDarkMode] = useState(true);
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                darkMode={dark}
+                toggleDarkMode={()=>{ setDarkMode(prevMode => !prevMode) }}
+            />
+            <Main darkMode={dark}/>
         </div>
     )
 }
