32c4f7f34530005b0e56a6506f661b769880043c
b-luis <bscarletluis@gmail.com>
2023-08-28 23:17:50 +0800

    fix: display dice elements - separate variable

--- 06-tenzies-game/src/App.jsx
+++ 06-tenzies-game/src/App.jsx
@@ -3,19 +3,9 @@ import { Die, Header } from "../src/components";
 import { nanoid } from "nanoid";
 
 function App() {
-	/**
-	 * Challenge: Update the `rollDice` function to not just roll
-	 * all new dice, but instead to look through the existing dice
-	 * to NOT role any that are being `held`.
-	 *
-	 * Hint: this will look relatively similiar to the `holdDice`
-	 * function below. When creating new dice, remember to use
-	 * `id: nanoid()` so any new dice have an `id` as well.
-	 */
-
-	// ? reference:
-	// https://stackoverflow.com/questions/46056998/how-add-item-in-an-object
 	const allNewDice = () => {
+		// ? reference:
+		// https://stackoverflow.com/questions/46056998/how-add-item-in-an-object
 		const newDice = [];
 		for (let i = 0; i < 10; i++) {
 			newDice.push({
@@ -47,13 +37,15 @@ function App() {
 		);
 	};
 
+	const displayDieEl = dice.map((die) => (
+		<Die key={die.id} {...die} onClick={() => holdDice(die.id)} />
+	));
+
 	return (
 		<main className="container__center rounded-md bg-slate-200 p-5 md:p-16">
 			<Header />
 			<div className="mt-5 grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-5">
-				{dice.map((die) => (
-					<Die key={die.id} {...die} onClick={() => holdDice(die.id)} />
-				))}
+				{displayDieEl}
 			</div>
 			<button
 				className="mt-5 rounded-md bg-blue-700 px-20 py-5 text-white shadow-md hover:bg-blue-900"
