84a3014c262ba7b6d830d8dffc6337679d278ee7
b-luis <bscarletluis@gmail.com>
2023-08-28 22:34:41 +0800

    feat: holdDice function - logging ID

--- 06-tenzies-game/src/App.jsx
+++ 06-tenzies-game/src/App.jsx
@@ -1,43 +1,48 @@
 import { useState } from "react";
 import { Die, Header } from "../src/components";
+import { nanoid } from "nanoid";
 
 function App() {
 	/**
-	 * Challenge: Update the array of numbers in state to be
-	 * an array of objects instead. Each object should look like:
-	 * { value: <random number>, isHeld: false }
+	 * Challenge: Create a function `holdDice` that takes
+	 * `id` as a parameter. For now, just have the function
+	 * console.log(id).
 	 *
-	 * Making this change will break parts of our code, so make
-	 * sure to update things so we're back to a working state
+	 * Then, figure out how to pass that function down to each
+	 * instance of the Die component so when each one is clicked,
+	 * it logs its own unique ID property. (Hint: there's more
+	 * than one way to make that work, so just choose whichever
+	 * you want)
+	 *
 	 */
 
 	// ? reference:
 	// https://stackoverflow.com/questions/46056998/how-add-item-in-an-object
-
 	const allNewDice = () => {
 		const newDice = [];
 		for (let i = 0; i < 10; i++) {
 			newDice.push({
 				value: Math.ceil(Math.random() * 6),
-				isHeld: false
+				isHeld: false,
+				id: nanoid()
 			});
 		}
 		return newDice;
 	};
 
 	const [dice, setDice] = useState(allNewDice());
+	const rollDice = () => setDice(allNewDice());
 
-	const rollDice = (e) => {
-		e.preventDefault();
-		setDice(allNewDice());
+	const holdDice = (id) => {
+		console.log(id);
 	};
 
 	return (
 		<main className="container__center rounded-md bg-slate-200 p-5 md:p-16">
 			<Header />
 			<div className="mt-5 grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-5">
-				{dice.map((die, i) => (
-					<Die key={i} value={die.value} isHeld={die.isHeld} />
+				{dice.map((die) => (
+					<Die key={die.id} {...die} onClick={() => holdDice(die.id)} />
 				))}
 			</div>
 			<button
--- 06-tenzies-game/src/components/Die.jsx
+++ 06-tenzies-game/src/components/Die.jsx
@@ -1,10 +1,9 @@
 function Die(props) {
+	const changeColor = props.isHeld ? "bg-[#59E391]" : "bg-white";
 	return (
 		<button
 			onClick={props.onClick}
-			className={`place-items-center rounded-md ${
-				props.isHeld ? "bg-[#59E391]" : "bg-white"
-			} px-10 py-5 font-bold shadow-md md:text-xl lg:text-3xl`}
+			className={`place-items-center rounded-md ${changeColor} px-10 py-5 font-bold shadow-md md:text-xl lg:text-3xl`}
 		>
 			{props.value}
 		</button>
